faa2404868d0c2b451336730b27f42f8e95c89c1
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-12 08:50:34 -0500

    separated two pieces of intended api

--- intended/index.js
+++ intended/index.js
@@ -1,19 +1,11 @@
-var u, isObject = require('@timelaps/is/object'),
-    isArray = require('@timelaps/is/array'),
-    forEach = require('@timelaps/hacks/for/each'),
+var u, forEach = require('@timelaps/hacks/for/each'),
     reverseParams = require('../reverse-params'),
-    forOwn = require('@timelaps/hacks/for/in/has');
-module.exports = function intended(key, value, fn) {
-    var obj;
-    if (isArray(key)) {
-        forEach(key, function (first) {
-            fn(first, value);
-        });
-    } else {
-        if ((obj = isObject(key) ? key : u)) {
-            forOwn(obj, reverseParams(fn));
-        } else {
-            fn(key, value);
-        }
-    }
-};
+    forOwn = require('@timelaps/hacks/for/in/has'),
+    maker = require('./maker');
+module.exports = maker(function (key, value, fn) {
+    forEach(key, function (key) {
+        fn(key, value);
+    });
+}, function (obj, fn) {
+    forOwn(obj, reverseParams(fn));
+});
