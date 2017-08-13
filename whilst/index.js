3fa8c3af1425952c8d497bfcb4e5300094d1b8c8
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-12 22:07:01 -0500

    updated whilst to track index

--- whilst/index.js
+++ whilst/index.js
@@ -1,7 +1,9 @@
 module.exports = function whilst(filter, continuation, _memo) {
-    var memo = _memo;
-    while (filter(memo)) {
-        memo = continuation(memo);
+    var memo = _memo,
+        counter = 0;
+    while (filter(memo, counter)) {
+        memo = continuation(memo, counter);
+        counter += 1;
     }
     return memo;
 };
