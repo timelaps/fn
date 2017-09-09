81d56fd50e7578f9951078b840ae53f355e6b028
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-09-09 14:39:19 -0500

    refactored generator to be a bit clearer

--- generator/index.js
+++ generator/index.js
@@ -1,21 +1,29 @@
 var returnsSecondArgument = require('@timelaps/returns/second');
 var returnsFirstArgument = require('@timelaps/returns/first');
-module.exports = function arrayGenerator(array, dir_, cap_, incrementor_, transformer_) {
+module.exports = gen;
+
+function gen(array, dir_, cap_, incrementor_, transformer_) {
     var previous, dir = dir_ || 1,
         length = array.length,
         counter = dir > 0 ? -1 : length,
         transformer = transformer_ || returnsFirstArgument,
         incrementor = incrementor_ || returnsSecondArgument,
-        cap = cap_ || (counter < 0 ? function (counter) {
-            return counter >= length;
-        } : function (counter) {
-            return counter < 0;
-        });
-    return function generateNext(fn) {
+        cap = cap_ || (counter < 0 ? untilLength : until0);
+    return next;
+
+    function next() {
         counter += dir;
         if (cap(counter)) {
             return;
         }
         return transformer(previous = incrementor(previous, counter, array));
-    };
-};
+    }
+
+    function untilLength(counter) {
+        return counter >= length;
+    }
+
+    function until0(counter) {
+        return counter < 0;
+    }
+}
