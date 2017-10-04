d770e002b4077bbeec7ec94d30add4f761c9d0f2
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-10-03 23:07:37 -0500

    updated defer to be more readable

--- defer/index.js
+++ defer/index.js
@@ -1,13 +1,27 @@
 var toArrayFromArrayLike = require('@timelaps/to/array/from/array-like');
-module.exports = function defer(fn, time, context) {
+module.exports = defer;
+
+function defer(fn, time, context) {
     var id;
-    return function deferInstance() {
+    defer.bypass = bypass;
+    return defer;
+
+    function bypass(context, args) {
+        clearTimeout(id);
+        finish(context, args);
+    }
+
+    function finish(context, args) {
+        fn.apply(context, args);
+    }
+
+    function defer() {
         var context = context || this,
             args = toArrayFromArrayLike(arguments);
         clearTimeout(id);
-        id = setTimeout(function deferTimer() {
-            fn.apply(context, args);
+        id = setTimeout(function () {
+            finish(context, args);
         }, time);
         return id;
-    };
-};
+    }
+}
