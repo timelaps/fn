f33fd3784707cfd114332b78b39839e7138d4b4e
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-10-14 12:58:23 -0500

    updated name to signify json instead of copy

---
+++ cacheable/json/index.js
@@ -0,0 +1,24 @@
+module.exports = cacheableCopy;
+
+var u, _stringify = require('@timelaps/json/stringify');
+var _parse = require('@timelaps/json/parse');
+
+function cacheableCopy(fn, stringify_, parse_) {
+    var stringify = stringify_ || _stringify;
+    var parse = parse_ || _parse;
+    var cache = {};
+    cacheableCopyInstance.cache = cache;
+    return cacheableCopyInstance;
+
+    function cacheableCopyInstance(string) {
+        var result, cached;
+        if (!(cached = cache[string])) {
+            result = fn(string);
+            if (result !== u) {
+                cached = cache[string] = stringify(result);
+            }
+            cached = result;
+        }
+        return cached ? parse(cached) : cached;
+    }
+}
